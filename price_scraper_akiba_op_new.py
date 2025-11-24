38f651fe1532ee26b77fdbd7a1e58b4907120c6b
Bryan Yau <bryanyau@successbox2015.com>
2025-11-24 17:07:53 +0800

    24/11

--- price_scraper_akiba_op_new.py
+++ price_scraper_akiba_op_new.py
@@ -11,6 +11,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request 
 import os.path
+import sys
 import time
 import re
 import random
